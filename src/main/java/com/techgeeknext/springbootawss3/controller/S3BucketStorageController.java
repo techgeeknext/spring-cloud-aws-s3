8c2e6978f165c87b66d2e098960f3bd9b64d1ab6
TechGeekNeXt <techgeeknext@gmail.com>
2021-05-17 15:05:05 +0530

    https://www.techgeeknext.com/cloud/aws/spring-cloud-aws-s3

---
+++ src/main/java/com/techgeeknext/springbootawss3/controller/S3BucketStorageController.java
@@ -0,0 +1,60 @@
+package com.techgeeknext.springbootawss3.controller;
+
+import com.techgeeknext.springbootawss3.service.S3BucketStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+@RestController
+public class S3BucketStorageController {
+    @Autowired
+    S3BucketStorageService service;
+
+    @GetMapping("/list/files")
+    public ResponseEntity<List<String>> getListOfFiles() {
+        return new ResponseEntity<>(service.listFiles(), HttpStatus.OK);
+    }
+
+    @PostMapping("/file/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("fileName") String fileName,
+                                             @RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(fileName, file), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/download/{filename}")
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String filename) {
+        ByteArrayOutputStream downloadInputStream = service.downloadFile(filename);
+
+        return ResponseEntity.ok()
+                .contentType(contentType(filename))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
+                .body(downloadInputStream.toByteArray());
+    }
+
+    @GetMapping(value = "/delete/{filename}")
+    public ResponseEntity<String> deleteFile(@PathVariable("filename") String filename) {
+        return new ResponseEntity<>(service.deleteFile(filename), HttpStatus.OK);
+    }
+
+    private MediaType contentType(String filename) {
+        String[] fileArrSplit = filename.split("\\.");
+        String fileExtension = fileArrSplit[fileArrSplit.length - 1];
+        switch (fileExtension) {
+            case "txt":
+                return MediaType.TEXT_PLAIN;
+            case "png":
+                return MediaType.IMAGE_PNG;
+            case "jpg":
+                return MediaType.IMAGE_JPEG;
+            default:
+                return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
+}
